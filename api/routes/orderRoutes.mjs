71465c77638a5c451b36b8d91d29edb78f497732
sudarshanrj07 <srjalawadi07@gmail.com>
2024-12-21 16:10:22 +0530

    added order payment route

--- api/routes/orderRoutes.mjs
+++ api/routes/orderRoutes.mjs
@@ -5,6 +5,7 @@ import { Order } from "../models/Order.mjs";
 
 const router = Router();
 
+//order creation route
 router.post(
 	"/",
 	userAuth,
@@ -46,6 +47,71 @@ router.post(
 		}
 
 		res.status(200).json(saveOrder);
+	})
+);
+
+//order payment route
+router.put(
+	"/:id/payment",
+	userAuth,
+	asyncHandler(async (req, res) => {
+		const {
+			params: { id: paramId },
+			body: { id: bodyId, email_address, status, updated_time },
+		} = req;
+		const findOrder = await Order.findById(paramId);
+		if (!findOrder) {
+			res.status(400);
+			throw new Error("Order does not exists");
+		}
+		findOrder.isPaid = true;
+		findOrder.paidAt = Date.now();
+		findOrder.paymentResult = {
+			id: bodyId,
+			status,
+			updated_time,
+			email_address,
+		};
+
+		const saveOrderPayment = await findOrder.save();
+
+		if (!saveOrderPayment) {
+			res.status(404);
+			throw new Error("Order payment failed");
+		}
+
+		res.status(200).json(saveOrderPayment);
+	})
+);
+
+//get all the orders route
+router.get(
+	"/",
+	userAuth,
+	asyncHandler(async (req, res) => {
+		const { _id } = req.user;
+		const orders = await Order.find({ _id }.sort({ _id: -1 }));
+		if (!orders) {
+			res.status(404);
+			throw new Error("No orders found");
+		}
+
+		res.status(200).json(orders);
+	})
+);
+
+//get order by id
+router.get(
+	"/:id",
+	userAuth,
+	asyncHandler(async (req, res) => {
+		const { id } = req.params;
+		const order = await Order.findById(id).populate("user", "email");
+		if (!order) {
+			res.status(404);
+			throw new Error("Order doesnt exists");
+		}
+		res.status(200).json(order);
 	})
 );
 
