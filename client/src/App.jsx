8f040714d8df2e9d744d29d5245bea58dc2ce5fe
sudarshanrj07 <srjalawadi07@gmail.com>
2024-12-22 00:26:21 +0530

    improved UI and added product detail page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,140 +1,14 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
+
 import { Layout } from "./layouts/Layouts";
+import { Products } from "./components/Products";
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
 		<Layout>
-			<section class="text-gray-600 body-font">
-				<div class="container px-5 py-24 mx-auto">
-					<div class="flex flex-wrap -m-4">
-						<div class="p-4 lg:w-1/4 md:w-1/2">
-							<div class="bg-white">
-								<div class="max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-									<div class="mt-6 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-										<div class="group relative">
-											<img
-												src="https://tailwindui.com/plus/img/ecommerce-images/product-page-01-related-product-01.jpg"
-												alt="Front of men&#039;s Basic Tee in black."
-												class="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"
-											/>
-											<div class="mt-4 flex justify-between">
-												<div>
-													<h3 class="text-sm text-gray-700">
-														<a href="#">
-															<span
-																aria-hidden="true"
-																class="absolute inset-0"
-															></span>
-															Basic Tee
-														</a>
-													</h3>
-													<p class="mt-1 text-sm text-gray-500">Black</p>
-												</div>
-												<p class="text-sm font-medium text-gray-900">$35</p>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div class="p-4 lg:w-1/4 md:w-1/2">
-							<div class="bg-white">
-								<div class="max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-									<div class="mt-6 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-										<div class="group relative">
-											<img
-												src="https://tailwindui.com/plus/img/ecommerce-images/product-page-01-related-product-01.jpg"
-												alt="Front of men&#039;s Basic Tee in black."
-												class="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"
-											/>
-											<div class="mt-4 flex justify-between">
-												<div>
-													<h3 class="text-sm text-gray-700">
-														<a href="#">
-															<span
-																aria-hidden="true"
-																class="absolute inset-0"
-															></span>
-															Basic Tee
-														</a>
-													</h3>
-													<p class="mt-1 text-sm text-gray-500">Black</p>
-												</div>
-												<p class="text-sm font-medium text-gray-900">$35</p>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div class="p-4 lg:w-1/4 md:w-1/2">
-							<div class="bg-white">
-								<div class="max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-									<div class="mt-6 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-										<div class="group relative">
-											<img
-												src="https://tailwindui.com/plus/img/ecommerce-images/product-page-01-related-product-01.jpg"
-												alt="Front of men&#039;s Basic Tee in black."
-												class="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"
-											/>
-											<div class="mt-4 flex justify-between">
-												<div>
-													<h3 class="text-sm text-gray-700">
-														<a href="#">
-															<span
-																aria-hidden="true"
-																class="absolute inset-0"
-															></span>
-															Basic Tee
-														</a>
-													</h3>
-													<p class="mt-1 text-sm text-gray-500">Black</p>
-												</div>
-												<p class="text-sm font-medium text-gray-900">$35</p>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div class="p-4 lg:w-1/4 md:w-1/2">
-							<div class="bg-white">
-								<div class="max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-									<div class="mt-6 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-										<div class="group relative">
-											<img
-												src="https://tailwindui.com/plus/img/ecommerce-images/product-page-01-related-product-01.jpg"
-												alt="Front of men&#039;s Basic Tee in black."
-												class="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"
-											/>
-											<div class="mt-4 flex justify-between">
-												<div>
-													<h3 class="text-sm text-gray-700">
-														<a href="#">
-															<span
-																aria-hidden="true"
-																class="absolute inset-0"
-															></span>
-															Basic Tee
-														</a>
-													</h3>
-													<p class="mt-1 text-sm text-gray-500">Black</p>
-												</div>
-												<p class="text-sm font-medium text-gray-900">$35</p>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</section>
+			<Products></Products>
 		</Layout>
 	);
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Navigate,
+} from "react-router-dom";
+import ProducDetail from "./pages/ProductDetail.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<Router>
+			<Routes>
+				<Route exact path="/" element={<App />} />
+				<Route exact path="/detail" element={<ProducDetail />} />
+			</Routes>
+		</Router>
+	</StrictMode>
+);
